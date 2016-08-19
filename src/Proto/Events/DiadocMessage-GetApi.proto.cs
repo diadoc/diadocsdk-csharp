270a92a70cf80585fa796ba6076d0d3372302613
halex2005 <harl_aa@skbkontur.ru>
2016-08-19 17:26:54 +0500

    remove utd receipt and confirmation attachments, they are not supported

--- src/Proto/Events/DiadocMessage-GetApi.proto.cs
+++ src/Proto/Events/DiadocMessage-GetApi.proto.cs
@@ -750,11 +750,8 @@ namespace Diadoc.Api.Proto.Events
       [global::ProtoBuf.ProtoEnum(Name=@"UniversalTransferDocumentBuyerTitle", Value=42)]
       UniversalTransferDocumentBuyerTitle = 42,
             
-      [global::ProtoBuf.ProtoEnum(Name=@"UniversalTransferDocumentReceipt", Value=43)]
-      UniversalTransferDocumentReceipt = 43,
-            
-      [global::ProtoBuf.ProtoEnum(Name=@"UniversalTransferDocumentConfirmation", Value=44)]
-      UniversalTransferDocumentConfirmation = 44,
+      [global::ProtoBuf.ProtoEnum(Name=@"UniversalTransferDocumentRevision", Value=45)]
+      UniversalTransferDocumentRevision = 45,
             
       [global::ProtoBuf.ProtoEnum(Name=@"CustomData", Value=64)]
       CustomData = 64,
