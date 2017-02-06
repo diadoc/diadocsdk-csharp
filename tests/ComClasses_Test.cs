23839bf9224ddf82ec561f3909907220b2f61b6c
Alexander Svyazin <svyazin@skbkontur.ru>
2017-02-06 12:44:28 +0500

    moved some unit tests from big solution

---
+++ tests/ComClasses_Test.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+namespace Diadoc.Api.Tests
+{
+	[TestFixture]
+	public class ComClasses_Test
+	{
+		private readonly Assembly diadocApiAssembly = typeof(ComDiadocApi).Assembly;
+
+		[Test]
+		public void EachClassInheritedFromComVisibleInterface_MustBeComVisible()
+		{
+			var comInvisibleClassesWithComVisibleInterface = diadocApiAssembly.GetTypes()
+					.Where(x => x.IsClass && !IsComVisible(x) && FindComVisibleInterfaces(x).Any())
+					.ToArray();
+
+			foreach (var type in comInvisibleClassesWithComVisibleInterface)
+				Console.WriteLine(type.Name);
+
+			Assert.IsFalse(comInvisibleClassesWithComVisibleInterface.Any());
+		}
+
+		[Test]
+		public void ComDefaultInterfaceAttribute_MustPointToSingleInterface()
+		{
+			var comClasses = GetComClasses();
+			foreach (var comClass in comClasses)
+			{
+				var implementedInterface = FindComVisibleInterfaces(comClass).FirstOrDefault();
+				Assert.IsNotNull(implementedInterface, "Failed to get single com visible interface for class: {0}", comClass);
+				Console.Out.WriteLine("{0} implements {1}", comClass.Name, implementedInterface.Name);
+				var comDefaultInterface = TryGetComDefaultInterface(comClass);
+				Assert.That(implementedInterface, Is.EqualTo(comDefaultInterface));
+			}
+		}
+
+		[Test]
+		public void ClassInterfaceAttribute_MustBeSetToNone()
+		{
+			var comClasses = GetComClasses();
+			foreach (var comClass in comClasses)
+			{
+				var classInterface = FindAttributes<ClassInterfaceAttribute>(comClass).FirstOrDefault();
+				Assert.IsNotNull(classInterface, "ClassInterface attribute is not set for class: {0}", comClass);
+				Assert.That(classInterface.Value, Is.EqualTo(ClassInterfaceType.None));
+			}
+		}
+
+		[Test]
+		public void GuidAttributes_MustBeAllDistinct()
+		{
+			var typeGuids = diadocApiAssembly.GetTypes()
+				.Select(x => new { Guid = TryGetGuid(x), Type = x })
+				.Where(g => g.Guid.HasValue)
+				.ToList();
+			var guidTypeStrings = string.Join("\n", typeGuids
+				.GroupBy(x => x.Guid)
+				.Where(x => x.Count() > 1)
+				.SelectMany(x => x)
+				.Select(x => string.Format("{0} for {1}", x.Guid, x.Type.Name))
+				.ToArray());
+			Console.WriteLine(guidTypeStrings);
+			Assert.That(typeGuids.Select(x => x.Guid).Distinct().Count(), Is.EqualTo(typeGuids.Count));
+		}
+
+		[Test]
+		public void ComVisibleInterfaces_DoNotReturnArrayList()
+		{
+			var comInterfaces = diadocApiAssembly.GetTypes()
+				.Where(x => x.IsInterface && IsComVisible(x))
+				.ToArray();
+
+			bool hasErrors = false;
+			foreach (var comInterface in comInterfaces)
+			{
+				var wrongMembers = comInterface.GetMembers()
+					.Where(x => IsMethodReturnsType(x, typeof(ArrayList)))
+					.ToArray();
+				foreach (var wrongMember in wrongMembers)
+					Console.WriteLine("{0}.{1}", comInterface.Name, wrongMember.Name);
+				if (wrongMembers.Any())
+					hasErrors = true;
+			}
+			Assert.IsFalse(hasErrors);
+		}
+
+		private bool IsMethodReturnsType(MemberInfo member, Type returnType)
+		{
+			if (member is MethodInfo)
+				return ((MethodInfo)member).ReturnType == returnType;
+			if (member is PropertyInfo)
+				return ((PropertyInfo)member).PropertyType == returnType;
+			throw new InvalidOperationException();
+		}
+
+		private IEnumerable<Type> GetComClasses()
+		{
+			var comClasses = diadocApiAssembly.GetTypes()
+				.Where(x => x.IsClass && TryGetComDefaultInterface(x) != null)
+				.ToArray();
+			return comClasses;
+		}
+
+		private Type[] FindComVisibleInterfaces(Type type)
+		{
+			return type.GetInterfaces()
+				.Where(x => IsComVisible(x) && x.Assembly == diadocApiAssembly)
+				.ToArray();
+		}
+
+		private static bool IsComVisible(Type type)
+		{
+			return FindAttributes<ComVisibleAttribute>(type).Any(x => x.Value);
+		}
+
+		private static Type TryGetComDefaultInterface(Type type)
+		{
+			var attribute = FindAttributes<ComDefaultInterfaceAttribute>(type).FirstOrDefault();
+			return attribute == null ? null : attribute.Value;
+		}
+
+		private static Guid? TryGetGuid(Type type)
+		{
+			var attribute = FindAttributes<GuidAttribute>(type).FirstOrDefault();
+			return attribute == null ? (Guid?)null : new Guid(attribute.Value);
+		}
+
+		private static T[] FindAttributes<T>(Type type)
+		{
+			return type.GetCustomAttributes(typeof(T), false).Cast<T>().ToArray();
+		}
+	}
+}
---
+++ tests/ComEnumsAreCompatibleWithProtoEnums_Test.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using Diadoc.Api.Proto;
+using Diadoc.Api.Proto.Docflow;
+using Diadoc.Api.Proto.Documents;
+using Diadoc.Api.Proto.Documents.BilateralDocument;
+using Diadoc.Api.Proto.Documents.InvoiceDocument;
+using Diadoc.Api.Proto.Documents.NonformalizedDocument;
+using Diadoc.Api.Proto.Documents.UnilateralDocument;
+using Diadoc.Api.Proto.Documents.UniversalTransferDocument;
+using Diadoc.Api.Proto.Events;
+using Diadoc.Api.Proto.Invoicing;
+using Diadoc.Api.Proto.Invoicing.Organizations;
+using Diadoc.Api.Proto.Invoicing.Signers;
+using NUnit.Framework;
+
+namespace Diadoc.Api.Tests
+{
+	[TestFixture]
+	public class ComEnumsAreCompatibleWithProtoEnums_Test
+	{
+		[Test]
+		public void Test_ReceiptStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.ReceiptStatus),
+				typeof(ReceiptStatus));
+		}
+
+		[Test]
+		public void Test_BilateralDocumentStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.BilateralDocumentStatus),
+				typeof(BilateralDocumentStatus));
+		}
+
+		[Test]
+		public void Test_RevocationStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.RevocationStatus),
+				typeof(RevocationStatus));
+		}
+
+		[Test]
+		public void Test_ResolutionRequestType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.ResolutionRequestType),
+				typeof(ResolutionRequestType));
+		}
+
+		[Test]
+		public void Test_UnilateralDocumentStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.UnilateralDocumentStatus),
+				typeof(UnilateralDocumentStatus));
+		}
+
+		[Test]
+		public void Test_UniversalTransferDocumentStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.UniversalTransferDocumentStatus),
+				typeof(UniversalTransferDocumentStatus));
+		}
+
+		[Test]
+		public void Test_InvoiceStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.InvoiceStatus),
+				typeof(InvoiceStatus));
+		}
+
+		[Test]
+		public void Test_ItemMark()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.ItemMark),
+				typeof(ItemMark));
+		}
+
+		[Test]
+		public void Test_TaxRate()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.TaxRate),
+				typeof(TaxRate));
+		}
+
+		[Test]
+		public void Test_EntityType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.EntityType),
+				typeof(EntityType));
+		}
+
+		[Test]
+		public void Test_FunctionType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.FunctionType),
+				typeof(FunctionType));
+		}
+
+		[Test]
+		public void Test_AttachmentType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.AttachmentType),
+				typeof(AttachmentType));
+		}
+
+		[Test]
+		public void Test_DocumentAccessLevel()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.DocumentAccessLevel),
+				typeof(DocumentAccessLevel));
+		}
+
+		[Test]
+		public void Test_DocumentType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.DocumentType),
+				typeof(DocumentType));
+		}
+
+		[Test]
+		public void Test_CertificateChainStatus()
+		{
+			AssertFirstEnumIsMoreOrEquals(
+				typeof(Com.CertificateChainStatus),
+				typeof(X509ChainStatusFlags));
+		}
+
+		[Test]
+		public void Test_NonformalizedDocumentStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.NonformalizedDocumentStatus),
+				typeof(NonformalizedDocumentStatus));
+		}
+
+		[Test]
+		public void Test_OrgType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.OrgType),
+				typeof(OrgType));
+		}
+
+		[Test]
+		public void Test_SearchScope()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.SearchScope),
+				typeof(SearchScope));
+		}
+
+		[Test]
+		public void Test_SignerPowers()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.SignerPowers),
+				typeof(SignerPowers));
+		}
+
+		[Test]
+		public void Test_SignerStatus()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.SignerStatus),
+				typeof(SignerStatus));
+		}
+
+		[Test]
+		public void Test_SignerType()
+		{
+			AssertEnumsAreCompatible(
+				typeof(Com.SignerType),
+				typeof(SignerType));
+		}
+
+		private static void AssertEnumsAreCompatible(Type comEnumType, Type protoEnumType)
+		{
+			var comEnumValues = GetEnumValues(comEnumType);
+			var protoEnumValues = GetEnumValues(protoEnumType);
+			Assert.That(comEnumValues, Is.EquivalentTo(protoEnumValues));
+
+			foreach (var valueName in protoEnumValues)
+			{
+				var comValue = (int)comEnumType.GetField(valueName).GetValue(null);
+				var protoValue = (int)protoEnumType.GetField(valueName).GetValue(null);
+				Assert.AreEqual(comValue, protoValue);
+			}
+		}
+
+		private static void AssertFirstEnumIsMoreOrEquals(Type first, Type second)
+		{
+			var firstValues = GetEnumValues(first);
+			var secondValues = GetEnumValues(second);
+
+			var subractedList = secondValues.Except(firstValues);
+			Assert.IsEmpty(subractedList);
+		}
+
+		private static string[] GetEnumValues(Type enumType)
+		{
+			return enumType
+				.GetMembers(BindingFlags.Public | BindingFlags.Static)
+				.Select(x => x.Name).ToArray();
+		}
+	}
+}
