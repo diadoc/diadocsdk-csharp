cf13c30b528c8fd9a06a0026030c5df7d629fd62
skukov <skukov@skbkontur.ru>
2017-07-12 09:53:41 +0500

    revert all AssemblyVersions

--- src/Properties/AssemblyVersion.cs
+++ src/Properties/AssemblyVersion.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.44.0.0")]
-[assembly: AssemblyFileVersion("1.44.1-dev")]
-[assembly: AssemblyInformationalVersion("1.44.1-dev")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0-dev")]
+[assembly: AssemblyInformationalVersion("1.0.0-dev")]
 
