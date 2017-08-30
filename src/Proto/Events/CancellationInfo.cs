dd5800ef9c13d7b98ec4b293a6113c1e1bbf0ff2
v.vasyliev <v.vasyliev@skbkontur.ru>
2017-08-30 15:28:13 +0500

    Added COM support

---
+++ src/Proto/Events/CancellationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Diadoc.Api.Proto.Events
+{
+	public interface ICancellationInfo
+	{
+		string Author { get; set; }
+	}
+
+	[ComVisible(true)]
+	[ProgId("Diadoc.Api.CancellationInfo")]
+	[Guid("621A5E43-B342-44DA-B0A5-33729949B654")]
+	[ClassInterface(ClassInterfaceType.None)]
+	[ComDefaultInterface(typeof(ICancellationInfo))]
+	public partial class CancellationInfo : SafeComObject, ICancellationInfo
+	{
+	}
+}
