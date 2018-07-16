41c000558ab74e489b30d187a71bc14b5515d7d2
v.vasyliev <v.vasyliev@skbkontur.ru>
2018-07-16 16:59:59 +0300

    Implemented generic sender title generation

--- src/Properties/AssemblyVersion.cs
+++ src/Properties/AssemblyVersion.cs
@@ -6,6 +6,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.51.0.0")]
-[assembly: AssemblyFileVersion("1.51.1-dev")]
-[assembly: AssemblyInformationalVersion("1.51.1-dev")]
+[assembly: AssemblyFileVersion("1.51.5-dev")]
+[assembly: AssemblyInformationalVersion("1.51.5-dev")]
 
