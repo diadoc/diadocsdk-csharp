ed5f3aa766536cc9be48f32d31f161e6ba351266
k.selivanov <k.selivanov@skbkontur.ru>
2019-08-20 14:52:25 +0300

    change auth to v3

--- src/DiadocHttpApi.Auth.cs
+++ src/DiadocHttpApi.Auth.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using Diadoc.Api.Http;
+using Diadoc.Api.Proto;
 
 namespace Diadoc.Api
 {
@@ -8,34 +9,58 @@ namespace Diadoc.Api
 	{
 		public string Authenticate(string login, string password, string key = null, string id = null)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/Authenticate");
-			qsb.AddParameter("login", login);
-			qsb.AddParameter("password", password);
+			var qsb = new PathAndQueryBuilder("/V3/Authenticate");
+			qsb.AddParameter("type", "password");
+
+			var request = BuildHttpRequest(
+				null,
+				"POST",
+				qsb.BuildPathAndQuery(),
+				Serialize(new LoginPassword
+				{
+					Login = login,
+					Password = password
+				}));
+
 			if (!string.IsNullOrEmpty(key))
 			{
-				qsb.AddParameter("key", key);
-				qsb.AddParameter("id", id);
+				request.AddHeader("X-Diadoc-ServiceKey", key);
+				request.AddHeader("X-Diadoc-ServiceUserId", id);
 			}
 
-			var httpResponse = PerformHttpRequest(null, "POST", qsb.BuildPathAndQuery());
-			return Encoding.UTF8.GetString(httpResponse);
+			return PerformRequest(request);
 		}
 
 		public string AuthenticateByKey(string key, string id)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/Authenticate");
-			qsb.AddParameter("key", key);
-			qsb.AddParameter("id", id);
-			var httpResponse = PerformHttpRequest(null, "POST", qsb.BuildPathAndQuery());
-			return Encoding.UTF8.GetString(httpResponse);
+			var qsb = new PathAndQueryBuilder("/V3/Authenticate");
+			qsb.AddParameter("type", "trust");
+			
+			var request = BuildHttpRequest(null,"POST", qsb.BuildPathAndQuery(),null);
+			
+			request.AddHeader("X-Diadoc-ServiceKey", key);
+			request.AddHeader("X-Diadoc-ServiceUserId", id);
+			
+			return PerformRequest(request);
+		}
+
+		private string PerformRequest(HttpRequest request)
+		{
+			var httpResponse = HttpClient.PerformHttpRequest(request);
+			return Encoding.UTF8.GetString(httpResponse.Content);
 		}
 
 		public string AuthenticateBySid(string sid)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/Authenticate");
-			qsb.AddParameter("sid", sid);
-			var httpResponse = PerformHttpRequest(null, "POST", qsb.BuildPathAndQuery());
-			return Encoding.UTF8.GetString(httpResponse);
+			var qsb = new PathAndQueryBuilder("/V3/Authenticate");
+			qsb.AddParameter("type", "sid");
+			var request = BuildRequest(
+				null,
+				"POST", 
+				qsb.BuildPathAndQuery(),
+				new HttpRequestBody(Encoding.UTF8.GetBytes(sid), "text/plain"));
+			
+			return PerformRequest(request);
 		}
 
 		public string Authenticate(byte[] certificateBytes, bool useLocalSystemStorage = false)
@@ -43,10 +68,10 @@ namespace Diadoc.Api
 			var token = AuthenticateByCertificate(
 				certificateBytes,
 				useLocalSystemStorage,
-				key: null,
-				id: null);
+				null,
+				null);
 
-			return ConfirmAuthenticationByCertificate(certificateBytes, token, saveBinding: false);
+			return ConfirmAuthenticationByCertificate(certificateBytes, token, false);
 		}
 
 		public string Authenticate(string thumbprint, bool useLocalSystemStorage = false)
@@ -56,23 +81,25 @@ namespace Diadoc.Api
 			var token = AuthenticateByCertificate(
 				userCert.RawData,
 				useLocalSystemStorage,
-				key: null,
-				id: null);
+				null,
+				null);
 
-			return ConfirmAuthenticationByCertificateThumbprint(userCert.Thumbprint, token, saveBinding: false);
+			return ConfirmAuthenticationByCertificateThumbprint(userCert.Thumbprint, token, false);
 		}
 
-		public string AuthenticateWithKey(byte[] certificateBytes, bool useLocalSystemStorage = false, string key = null, string id = null, bool autoConfirm = true)
+		public string AuthenticateWithKey(byte[] certificateBytes, bool useLocalSystemStorage = false,
+			string key = null, string id = null, bool autoConfirm = true)
 		{
 			var authenticationWithKey = !string.IsNullOrEmpty(key);
 			var token = AuthenticateByCertificate(certificateBytes, useLocalSystemStorage, key, id);
 
 			return autoConfirm
-				? ConfirmAuthenticationByCertificate(certificateBytes, token, saveBinding: authenticationWithKey)
+				? ConfirmAuthenticationByCertificate(certificateBytes, token, authenticationWithKey)
 				: token;
 		}
 
-		public string AuthenticateWithKey(string thumbprint, bool useLocalSystemStorage = false, string key = null, string id = null, bool autoConfirm = true)
+		public string AuthenticateWithKey(string thumbprint, bool useLocalSystemStorage = false, string key = null,
+			string id = null, bool autoConfirm = true)
 		{
 			var authenticationWithKey = !string.IsNullOrEmpty(key);
 			var userCert = crypt.GetCertificateWithPrivateKey(thumbprint, useLocalSystemStorage);
@@ -80,7 +107,7 @@ namespace Diadoc.Api
 			var token = AuthenticateByCertificate(userCert.RawData, useLocalSystemStorage, key, id);
 
 			return autoConfirm
-				? ConfirmAuthenticationByCertificateThumbprint(userCert.Thumbprint, token, saveBinding: authenticationWithKey)
+				? ConfirmAuthenticationByCertificateThumbprint(userCert.Thumbprint, token, authenticationWithKey)
 				: token;
 		}
 
@@ -94,28 +121,33 @@ namespace Diadoc.Api
 			return ConfirmAuthenticationByCertificateThumbprint(thumbprint, token, saveBinding);
 		}
 
-		private string AuthenticateByCertificate(byte[] certificateBytes, bool useLocalSystemStorage, string key, string id)
+		private string AuthenticateByCertificate(byte[] certificateBytes, bool useLocalSystemStorage, string key,
+			string id)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/Authenticate");
-			var authenticationWithKey = !string.IsNullOrEmpty(key);
-			if (authenticationWithKey)
-			{
-				qsb.AddParameter("key", key);
-				qsb.AddParameter("id", id);
-			}
-
-			return PerformHttpRequest(null,
-				"POST",
+			var qsb = new PathAndQueryBuilder("/V3/Authenticate");
+			qsb.AddParameter("type", "certificate");
+			
+			var request = BuildRequest(
+				null,
+				"POST", 
 				qsb.BuildPathAndQuery(),
-				certificateBytes,
-				responseContent => Convert.ToBase64String(crypt.Decrypt(responseContent, useLocalSystemStorage)));
+				new HttpRequestBody(certificateBytes, "application/octet-stream")); 
+
+			if (!string.IsNullOrEmpty(key))
+			{
+				request.AddHeader("X-Diadoc-ServiceKey", key);
+				request.AddHeader("X-Diadoc-ServiceUserId", id);
+			}
+			var httpResponse = HttpClient.PerformHttpRequest(request);
+			return Convert.ToBase64String(crypt.Decrypt(httpResponse.Content, useLocalSystemStorage));
 		}
 
 		private string ConfirmAuthenticationByCertificate(byte[] certificateBytes, string token, bool saveBinding)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/AuthenticateConfirm");
+			var qsb = new PathAndQueryBuilder("/V3/AuthenticateConfirm");
 			qsb.AddParameter("token", token);
 			qsb.AddParameter("saveBinding", saveBinding.ToString());
+			
 			return PerformHttpRequest(
 				null,
 				"POST",
@@ -126,7 +158,7 @@ namespace Diadoc.Api
 
 		private string ConfirmAuthenticationByCertificateThumbprint(string thumbprint, string token, bool saveBinding)
 		{
-			var qsb = new PathAndQueryBuilder("/V2/AuthenticateConfirm");
+			var qsb = new PathAndQueryBuilder("/V3/AuthenticateConfirm");
 			qsb.AddParameter("thumbprint", thumbprint);
 			qsb.AddParameter("token", token);
 			qsb.AddParameter("saveBinding", saveBinding.ToString());
