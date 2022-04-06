6023ed235f10d2a696bac50c2b3c04bb76bdd3b0
Dmitriy Ipatyev <ipatev@skbkontur.ru>
2022-04-06 10:25:03 +0500

    Add GetRoamingOperators method

---
+++ src/DiadocHttpApi.RoamingOperators.cs
@@ -0,0 +1,15 @@
+﻿using Diadoc.Api.Http;
+using Diadoc.Api.Proto;
+
+namespace Diadoc.Api
+{
+	public partial class DiadocHttpApi
+	{
+		public RoamingOperatorList GetRoamingOperators(string authToken, string boxId)
+		{
+			var queryString = new PathAndQueryBuilder("/GetRoamingOperators");
+			queryString.AddParameter("boxId", boxId);
+			return PerformHttpRequest<RoamingOperatorList>(authToken, "GET", queryString.BuildPathAndQuery());
+		}
+	}
+}
