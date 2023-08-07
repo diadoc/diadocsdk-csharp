aeb212b796e95bb73e68b6c140772de5eccca821
anatoli.dubko <anatoli.dubko@skbkontur.ru>
2023-08-07 17:59:16 +0500

    add new cloudcrypt confirmation type

---
+++ src/Com/DssConfirmType.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+namespace Diadoc.Api.Com
+{
+	[ComVisible(true)]
+	[Guid("7B7F18EB-E490-40C7-88F0-B653E8460590")]
+	//NOTE: Это хотели, чтобы можно было использовать XML-сериализацию для классов
+	[XmlType(TypeName = "DssConfirmType", Namespace = "https://diadoc-api.kontur.ru")]
+	public enum DssConfirmType
+	{
+		ConfirmTypeUnknown = -1,
+		None = 0,
+		Sms = 1,
+		MyDss = 2,
+		Applet = 3,
+		MobileSdk = 4
+	}
+}
