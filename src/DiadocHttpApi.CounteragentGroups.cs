be38a1dd09dcbff8cbe48b048a487b8c78b1a626
bukharmetova.dv <bukharmetova.dv@skbkontur.ru>
2024-02-13 12:54:20 +0700

    ddcore-8002: added ca group methods

---
+++ src/DiadocHttpApi.CounteragentGroups.cs
@@ -0,0 +1,31 @@
+﻿using Diadoc.Api.Http;
+using Diadoc.Api.Proto.CounteragentGroups;
+
+namespace Diadoc.Api
+{
+	public partial class DiadocHttpApi
+	{
+		public CounteragentGroup CreateCounteragentGroup(string authToken, string boxId, CounteragentGroupToCreate counteragentGroupToCreate)
+		{
+			var queryString = new PathAndQueryBuilder("/CreateCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			return PerformHttpRequest<CounteragentGroupToCreate, CounteragentGroup>(authToken, queryString.BuildPathAndQuery(), counteragentGroupToCreate);
+		}
+
+		public CounteragentGroup UpdateCounteragentGroup(string authToken, string boxId, string counteragentGroupId, CounteragentGroupToUpdate counteragentGroupToUpdate)
+		{
+			var queryString = new PathAndQueryBuilder("/UpdateCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			queryString.AddParameter("counteragentGroupId", counteragentGroupId);
+			return PerformHttpRequest<CounteragentGroupToUpdate, CounteragentGroup>(authToken, queryString.BuildPathAndQuery(), counteragentGroupToUpdate);
+		}
+
+		public void DeleteCounteragentGroup(string authToken, string boxId, string counteragentGroupId)
+		{
+			var queryString = new PathAndQueryBuilder("/DeleteCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			queryString.AddParameter("counteragentGroupId", counteragentGroupId);
+			PerformHttpRequest(authToken, "POST", queryString.BuildPathAndQuery());
+		}
+	}
+}
---
+++ src/DiadocHttpApi.CounteragentGroupsAsync.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Diadoc.Api.Http;
+using Diadoc.Api.Proto.CounteragentGroups;
+
+namespace Diadoc.Api
+{
+	public partial class DiadocHttpApi
+	{
+		public Task<CounteragentGroup> CreateCounteragentGroupAsync(string authToken, string boxId, CounteragentGroupToCreate counteragentGroupToCreate)
+		{
+			var queryString = new PathAndQueryBuilder("/CreateCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			return PerformHttpRequestAsync<CounteragentGroupToCreate, CounteragentGroup>(authToken, queryString.BuildPathAndQuery(), counteragentGroupToCreate);
+		}
+
+		public Task<CounteragentGroup> UpdateCounteragentGroupAsync(string authToken, string boxId, string counteragentGroupId, CounteragentGroupToUpdate counteragentGroupToUpdate)
+		{
+			var queryString = new PathAndQueryBuilder("/UpdateCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			queryString.AddParameter("counteragentGroupId", counteragentGroupId);
+			return PerformHttpRequestAsync<CounteragentGroupToUpdate, CounteragentGroup>(authToken, queryString.BuildPathAndQuery(), counteragentGroupToUpdate);
+		}
+
+		public Task DeleteCounteragentGroupAsync(string authToken, string boxId, string counteragentGroupId)
+		{
+			var queryString = new PathAndQueryBuilder("/DeleteCounteragentGroup");
+			queryString.AddParameter("boxId", boxId);
+			queryString.AddParameter("counteragentGroupId", counteragentGroupId);
+			return PerformHttpRequestAsync(authToken, "POST", queryString.BuildPathAndQuery());
+		}
+	}
+}
