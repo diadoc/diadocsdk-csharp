784b7dd990c19eaaca81c8164151879851a9a54f
belyaev.ia <belyaev.ia@kontur.ru>
2024-03-27 13:42:02 +0500

    add poa delegation info

--- Samples/Diadoc.Console/Properties/AssemblyVersion.cs
+++ Samples/Diadoc.Console/Properties/AssemblyVersion.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.12.0.0")]
-[assembly: AssemblyFileVersion("2.12.0-dev")]
-[assembly: AssemblyInformationalVersion("2.12.0-dev")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.7-dev")]
+[assembly: AssemblyInformationalVersion("2.14.7-dev")]
 
