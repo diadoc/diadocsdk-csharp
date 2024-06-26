95f9eee1a1e2a781c8a1eb7a7474a2e5e7336cfc
nm.suvorov <nm.suvorov@skbkontur.ru>
2024-06-26 13:49:29 +0300

    Added COM

---
+++ src/Com/StatusName.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+namespace Diadoc.Api.Com
+{
+	[ComVisible(true)]
+	[Guid("31005758-E76A-4E64-9BAA-8E78C380243B")]
+	[XmlType(TypeName = "StatusName", Namespace = "https://diadoc-api.kontur.ru")]
+	public enum StatusName
+	{
+		PowerOfAttorneyAttached = Proto.Docflow.StatusName.PowerOfAttorneyAttached,
+		PowerOfAttorneyNotRequired = Proto.Docflow.StatusName.PowerOfAttorneyNotRequired,
+		PowerOfAttorneyRequired = Proto.Docflow.StatusName.PowerOfAttorneyRequired
+	}
+}
