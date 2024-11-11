735458ec31d0f8ca97db3c82df567ae3a28eb173
Ya.komarov <ya.komarov@skbkontur.ru>
2024-11-11 16:37:47 +0500

    ddcore-9261: new shelf access methods added

--- src/DiadocHttpApi.Shelf.cs
+++ src/DiadocHttpApi.Shelf.cs
@@ -20,9 +20,10 @@ namespace Diadoc.Api
 			HttpStatusCode.PaymentRequired
 		};
 
-		public int ShelfUploadChunkSize { get { return partLength; } }
+		public int ShelfUploadChunkSize  { get { return partLength; } }
 		public int ShelfUploadMaxAttemptsCount { get { return maxAttempts; } }
 
+		[Obsolete("Use UploadFileToShelfV2 or UploadLargeFileToShelf")]
 		public string UploadFileToShelf(string authToken, byte[] data)
 		{
 			var nameOnShelf = string.Format("api-{0}", Guid.NewGuid());
@@ -41,6 +42,130 @@ namespace Diadoc.Api
 			return nameOnShelf;
 		}
 
+		public string UploadFileToShelfV2(string authToken, byte[] content, [CanBeNull] string fileExtension)
+		{
+			var httpErrors = new List<HttpClientException>();
+			var queryString = string.Format("V2/ShelfUpload?fileExtension={0}", fileExtension);
+
+			for (var i = 0; i < maxAttempts; i++)
+			{
+				try
+				{
+					var request = BuildRequest(authToken, "POST", queryString, new HttpRequestBody(content));
+					var response = HttpClient.PerformHttpRequest(request);
+					return Encoding.UTF8.GetString(response.Content);
+				}
+				catch (HttpClientException e)
+				{
+					if (e.ResponseStatusCode.HasValue && nonRetriableStatusCodes.Contains(e.ResponseStatusCode.Value)) throw;
+					httpErrors.Add(e);
+				}	
+			}
+
+			throw new AggregateException("Reached the limit of attempts to send a file", httpErrors.ToArray());
+		}
+
+		public string UploadLargeFileToShelf(string authToken, byte[] content, [CanBeNull] string fileExtension)
+		{
+			var parts = SplitDataIntoParts(content).ToList();
+
+			var httpErrors = new List<HttpClientException>();
+			var attempts = 0;
+			var missingParts = Enumerable.Range(0, parts.Count).ToList();
+			string fileName = null;
+
+			while (missingParts.Count > 0)
+			{
+				if (++attempts > maxAttempts)
+				{
+					throw new AggregateException("Reached the limit of attempts to send a file", httpErrors.ToArray());
+				}
+
+				if (fileName == null)
+				{
+					fileName = ShelfUploadPartInit(authToken, parts[0], missingParts, httpErrors, fileExtension);
+				}
+
+				if (fileName != null)
+				{
+					missingParts = ShelfUploadParts(authToken, fileName, parts, missingParts, httpErrors);
+				}
+			}
+
+			return fileName;
+		}
+
+		[CanBeNull]
+		private string ShelfUploadPartInit(string authToken, ArraySegment<byte> firstPart, [NotNull] IList<int> missingParts, [NotNull] ICollection<HttpClientException> httpErrors, [CanBeNull] string fileExtension)
+		{
+			var queryString = string.Format("ShelfUploadPartInit?fileExtension={0}", fileExtension);
+			if (missingParts.Count == 1)
+			{
+				queryString += "&isLastPart=true";
+			}
+			
+			try
+			{
+				var request = BuildRequest(authToken, "POST", queryString, new HttpRequestBody(firstPart));
+				var response = HttpClient.PerformHttpRequest(request);
+				missingParts.Remove(0);
+
+				return Encoding.UTF8.GetString(response.Content);
+			}
+			catch (HttpClientException e)
+			{
+				if (e.ResponseStatusCode.HasValue && nonRetriableStatusCodes.Contains(e.ResponseStatusCode.Value)) throw;
+				httpErrors.Add(e);
+			}
+
+			return null;
+		}
+
+		[NotNull]
+		private List<int> ShelfUploadParts(string authToken, string fileName, [NotNull] IList<ArraySegment<byte>> parts, [NotNull] IList<int> missingParts, [NotNull] ICollection<HttpClientException> httpErrors)
+		{
+			var currentMissingParts = missingParts.Count == 0 ? new List<int>() : null;
+			for (var i = 0; i < missingParts.Count; ++i)
+			{
+				var partIndex = missingParts[i];
+				currentMissingParts = UploadPart(authToken, fileName, parts[partIndex], partIndex, i == missingParts.Count - 1, httpErrors);
+			}
+
+			if (currentMissingParts == null)
+			{
+				throw new Exception("UploadPart did not return missing parts");
+			}
+
+			return currentMissingParts;
+		}
+
+		[CanBeNull]
+		private List<int> UploadPart(string authToken, string fileName, ArraySegment<byte> part, int partIndex, bool isLast, ICollection<HttpClientException> httpErrors)
+		{
+			var queryString = string.Format("ShelfUploadPart?fileName={0}&partIndex={1}&isLastPart={2}", fileName, partIndex, isLast);
+			try
+			{
+				var request = BuildRequest(authToken, "POST", queryString, new HttpRequestBody(part));
+				var response = HttpClient.PerformHttpRequest(request);
+				if (isLast)
+				{
+					var responseString = Encoding.UTF8.GetString(response.Content);
+					return responseString
+						.Split(new[] { ',', '[', ']' }, StringSplitOptions.RemoveEmptyEntries)
+						.Select(int.Parse)
+						.ToList();
+				}
+
+				return null;
+			}
+			catch (HttpClientException e)
+			{
+				if (e.ResponseStatusCode.HasValue && nonRetriableStatusCodes.Contains(e.ResponseStatusCode.Value)) throw;
+				httpErrors.Add(e);
+				return isLast ? new List<int> { partIndex } : null;
+			}
+		}
+
 		[NotNull]
 		private int[] PutMissingParts(string authToken, string nameOnShelf, [NotNull] IList<ArraySegment<byte>> allParts, [NotNull] IList<int> missingParts, [NotNull] ICollection<HttpClientException> httpErrors)
 		{
@@ -55,11 +180,18 @@ namespace Diadoc.Api
 			return currentMissingParts;
 		}
 
+		[Obsolete("Use GetFileFromShelfV2")]
 		public byte[] GetFileFromShelf(string authToken, string nameOnShelf)
 		{
 			if (!nameOnShelf.Contains("__userId__"))
 				nameOnShelf = string.Format("__userId__/{0}", nameOnShelf);
 			var queryString = string.Format("ShelfDownload?nameOnShelf={0}", nameOnShelf);
+			return PerformHttpRequest(authToken, "GET", queryString);
+		}
+
+		public byte[] GetFileFromShelfV2(string authToken, string fileName)
+		{
+			var queryString = $"V2/ShelfDownload?fileName={fileName}";
 			return PerformHttpRequest(authToken, "GET", queryString);
 		}
 
