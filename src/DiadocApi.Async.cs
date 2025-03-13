1c74acd10eda846c826d161fad541ddd9edc39c9
nm.suvorov <nm.suvorov@skbkontur.ru>
2025-03-13 13:14:09 +0500

    Added V2/DssSign

--- src/DiadocApi.Async.cs
+++ src/DiadocApi.Async.cs
@@ -1197,6 +1197,14 @@ namespace Diadoc.Api
 			if (request == null) throw new ArgumentNullException("request");
 			return diadocHttpApi.DssSignAsync(authToken, boxId, request, certificateThumbprint);
 		}
+		
+		[Obsolete("Currently unavailable. Use DssSignAsync")]
+		public Task<AsyncMethodResult> DssSignV2Async(string authToken, string boxId, DssSignRequestV2 request)
+		{
+			if (string.IsNullOrEmpty(boxId)) throw new ArgumentNullException("boxId");
+			if (request == null) throw new ArgumentNullException("request");
+			return diadocHttpApi.DssSignV2Async(authToken, boxId, request);
+		}
 
 		public Task<DssSignResult> DssSignResultAsync(string authToken, string boxId, string taskId)
 		{
