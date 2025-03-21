7febd7be5e8a74e2e993d6c834220f1c9904b334
Konstatin <16274436+koheatel@users.noreply.github.com>
2025-03-21 10:31:20 +0700

    DDS-65442: Add missing properties to IBoxEvent, IMessage COM objects (#1206)

--- src/Proto/Events/DiadocMessage.cs
+++ src/Proto/Events/DiadocMessage.cs
@@ -32,6 +32,13 @@ namespace Diadoc.Api.Proto.Events
 		DateTime Timestamp { get; }
 		DateTime LastPatchTimestamp { get; }
 		MessageType MessageTypeValue { get; }
+		bool IsProxified { get; }
+		string ProxyBoxId { get; }
+		string ProxyTitle { get; }
+		bool PacketIsLocked { get; }
+		Diadoc.Api.Com.LockMode LockModeValue { get; }
+		TemplateToLetterTransformationInfo TemplateToLetterTransformationInfo { get; }
+		bool IsReusable { get; }
 	}
 
 	[ComVisible(true)]
@@ -64,6 +71,32 @@ namespace Diadoc.Api.Proto.Events
 		{
 			get { return (MessageType)MessageType; }
 		}
+
+		public Diadoc.Api.Com.LockMode LockModeValue
+		{
+			get { return (Diadoc.Api.Com.LockMode)LockMode; }
+		}
+	}
+
+	[ComVisible(true)]
+	[Guid("F2B62A56-262C-4FCB-B9E8-80F6B1C46590")]
+	public interface ITemplateToLetterTransformationInfo
+	{
+		string LetterFromBoxId { get; }
+		string LetterToBoxId { get; }
+		string LetterFromDepartmentId { get; }
+		string LetterToDepartmentId { get; }
+		string LetterProxyBoxId { get; }
+		string LetterProxyDepartmentId { get; }
+	}
+
+	[ComVisible(true)]
+	[Guid("EAA0C98F-078B-4615-BE73-B225B64EBBA3")]
+	[ClassInterface(ClassInterfaceType.None)]
+	[ComDefaultInterface(typeof(ITemplateToLetterTransformationInfo))]
+	public partial class TemplateToLetterTransformationInfo : SafeComObject, ITemplateToLetterTransformationInfo
+	{
+		
 	}
 
 	[ComVisible(true)]
@@ -213,6 +246,7 @@ namespace Diadoc.Api.Proto.Events
 		bool HasPatch { get; }
 		Message Message { get; }
 		MessagePatch Patch { get; }
+		string IndexKey { get; }
 	}
 
 	[ComVisible(true)]
