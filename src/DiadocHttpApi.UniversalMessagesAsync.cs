629a2b1556bc19ad400c0d5040033ae33de2c7f2
yatsenko <yatsenko@skbkontur.ru>
2025-06-10 15:56:25 +0500

    ddcore-10257: universal message parse

--- src/DiadocHttpApi.UniversalMessagesAsync.cs
+++ src/DiadocHttpApi.UniversalMessagesAsync.cs
@@ -17,5 +17,27 @@ namespace Diadoc.Api
 			var response = await HttpClient.PerformHttpRequestAsync(request).ConfigureAwait(false);
 			return new GeneratedFile(response.ContentDispositionFileName, response.Content);
 		}
+		
+		public async Task<byte[]> ParseUniversalMessageAsync(string authToken, string boxId, string messageId, string attachmentId)
+		{
+			var queryBuilder = new PathAndQueryBuilder("/ParseUniversalMessage");
+			queryBuilder.AddParameter("boxId", boxId);
+			queryBuilder.AddParameter("messageId", messageId);
+			queryBuilder.AddParameter("attachmentId", attachmentId);
+
+			var request = BuildHttpRequest(authToken, "POST", queryBuilder.BuildPathAndQuery(), null);
+			var response = await HttpClient.PerformHttpRequestAsync(request).ConfigureAwait(false);
+			return response.Content;
+		}
+
+		public async Task<byte[]> ParseUniversalMessageXmlAsync(string authToken, byte[] content)
+		{
+			var queryBuilder = new PathAndQueryBuilder("/ParseUniversalMessageXml");
+
+			var request = BuildHttpRequest(authToken, "POST", queryBuilder.BuildPathAndQuery(), content);
+			var response = await HttpClient.PerformHttpRequestAsync(request).ConfigureAwait(false);
+
+			return response.Content;
+		}
 	}
 }
