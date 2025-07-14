1da6c57b3588a13771a01f4313e39c2cdc3dc26c
and.av <and.av@skbkontur.ru>
2025-07-14 16:09:28 +0500

    DDCORE-10318 add new data fields in msk time zone in poa model

---
+++ src/Proto/DateTimeOffsetDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Diadoc.Api.Proto
+{
+	public partial class DateTimeOffsetDto
+	{
+		public DateTimeOffset ToDateTimeOffset()
+		{
+			return new DateTimeOffset(UtcTicks, TimeSpan.Zero).ToOffset(TimeSpan.FromTicks(OffsetTicks));
+		}
+
+		public static explicit operator DateTimeOffset(DateTimeOffsetDto value) => value.ToDateTimeOffset();
+
+		public static explicit operator DateTimeOffsetDto(DateTimeOffset value)
+			=> new DateTimeOffsetDto
+			{
+				UtcTicks = value.UtcTicks,
+				OffsetTicks = value.Offset.Ticks
+			};
+	}
+}
