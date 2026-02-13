57faffde5096409eadd8d89c5fb3604aa771f265
Dengoan969 <92169913+Dengoan969@users.noreply.github.com>
2026-02-13 12:09:59 +0500

    DDCORE-11067: fix typo

--- src/DiadocApi.Async.cs
+++ src/DiadocApi.Async.cs
@@ -1747,7 +1747,7 @@ namespace Diadoc.Api
 			return diadocHttpApi.DetectCustomPrintFormsAsync(authToken, boxId, request);
 		}
 
-		[Obsolete("Use GetLastEventV2()")]
+		[Obsolete("Use GetLastEventV2Async()")]
 		public Task<BoxEvent> GetLastEventAsync(string authToken, string boxId)
 		{
 			if (authToken == null) throw new ArgumentNullException("authToken");
