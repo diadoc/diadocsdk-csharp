60e88a0afe6e118411a4ce02cf458837c56e8df7
d.smirnov <d.smirnov@skbkontur.ru>
2026-02-25 19:01:32 +0500

    feat: support COM

---
+++ src/Com/UniversalMessageBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Diadoc.Api.Com
+{
+	[ComVisible(true)]
+	[Guid("527D69FA-60D9-4D8E-8F40-7274371979C5")]
+	public enum UniversalMessageBehaviour
+	{
+		Undefined = Proto.UniversalMessageBehaviour.Undefined,
+		AffectsWorkflow = Proto.UniversalMessageBehaviour.AffectsWorkflow,
+		DoesNotAffectWorkflow = Proto.UniversalMessageBehaviour.DoesNotAffectWorkflow,
+	}
+}
